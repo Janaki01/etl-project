fd209fa451e6e6dd5ca7010cbb30d626313cbe94
muthuluri-janaki.muthuluri-janaki <you@example.com>
2026-01-20 22:40:53 +0530

    fix sql file

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -1,16 +1,17 @@
 import os
 from etl.db import get_engine
 
-SQL_DIR = os.path.join(os.path.dirname(__file__),".","sql")
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SQL_DIR = os.path.join(BASE_DIR,"sql")
 SQL_FILES_IN_ORDER = [
     "ABC_Sales_Database_Schema.sql"
 ]
-def run_sql_file(conn, filepath):
-    print("Running {filepath} ...")
-    with open(filepath, "r", encoding="utf-8") as f:
-        sql = f.read()
-        conn.exec_driver_sql(sql)
-    print(f"Finished {filepath}")
+# def run_sql_file(conn, filepath):
+#     print("Running {filepath} ...")
+#     with open(filepath, "r", encoding="utf-8") as f:
+#         sql = f.read()
+#         conn.exec_driver_sql(sql)
+#     print(f"Finished {filepath}")
 
 def init_db():
     engine= get_engine()
@@ -20,7 +21,11 @@ def init_db():
 
             if not os.path.exists(path):
                 raise FileNotFoundError(f"SQL file not found: {path}")
-            run_sql_file(conn, path)
+            with open(path, "r", encoding="utf-8") as f:
+                sql = f.read()
+                conn.exec_driver_sql(sql)
+            print(f"Executed: {filename}")
+
     print("PostgreSQL schema initialized successfully")
 
 if __name__=="__main__":
